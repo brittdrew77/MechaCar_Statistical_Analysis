19528dab399ac3866b5e8f16cbd96000be2db536
brittdrew77 <105089651+brittdrew77@users.noreply.github.com>
2022-08-24 16:25:10 -0400

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,13 @@
+# Deliverable 1
 library(dplyr)
 MechaCar_mpg <- read_csv("MechaCar_mpg.csv")
-lm(MechaCar_mpg)
-summary(lm(MechaCar_mpg))
+model = lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
+print(model)
+summary(model)
+
+#Deliverable 2
+Suspension_Coil <- read_csv("Suspension_Coil.csv")
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+view(total_summary)
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+view(lot_summary)
