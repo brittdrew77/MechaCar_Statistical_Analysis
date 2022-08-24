81bc8ed20b0fb0d2e0d13da702e4248262f4863a
brittdrew77 <105089651+brittdrew77@users.noreply.github.com>
2022-08-24 16:54:05 -0400

    deliverable 3: t-tests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -11,3 +11,9 @@ total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median
 view(total_summary)
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 view(lot_summary)
+
+#Deliverable 3
+t.test(Suspension_Coil$PSI, mu = 1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1", select = "PSI"), mu = 1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot2", select = "PSI"), mu = 1500)
+t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot3", select = "PSI"), mu = 1500)
